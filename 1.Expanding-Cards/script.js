3414d9b6e06a9914e1c82e5a5fb2c887e36881ba
Sharath S <17112360+ki11e6@users.noreply.github.com>
2023-03-09 11:41:04 +0530

    day1 expanding cards finished

---
+++ 1.Expanding-Cards/script.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll('.panel');
+// console.log(panels[1]);
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
